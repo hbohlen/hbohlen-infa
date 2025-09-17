40656218b3779f356d63e85613d494e6bc57bf1b
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-16 22:18:22 -0500

    Add disko module to NixOS configuration

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,20 +1,19 @@
-# flake.nix
 {
   description = "Hayden's NixOS configuration for Zephyrus M16";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    disko.url = "github:nix-community/disko";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }: {
+  outputs = { self, nixpkgs, nixos-hardware, disko, ... }: {
     nixosConfigurations = {
-      # This name "zephyrus-m16" is what you'll use in the rebuild command
       "zephyrus-m16" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          # Pass the hardware module to the configuration
-          nixos-hardware.nixosModules.asus-zephyrus-g603h
+          disko.nixosModules.disko
+          ./disko-config.nix
           ./configuration.nix
         ];
       };
